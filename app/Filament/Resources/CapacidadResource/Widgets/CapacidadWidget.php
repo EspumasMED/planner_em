88f2656742e77e3942e55a588146aad0b5f7094a
fonsi3000 <luiscarrascal@cun.edu.co>
2024-08-24 14:06:59 -0500

    se actualiza repositorio

--- app/Filament/Resources/CapacidadResource/Widgets/CapacidadWidget.php
+++ app/Filament/Resources/CapacidadResource/Widgets/CapacidadWidget.php
@@ -9,24 +9,6 @@ class CapacidadWidget extends Widget
 {
     protected static string $view = 'filament.resources.capacidad-resource.widgets.capacidad-widget';
 
-<<<<<<< HEAD
-    public function viewData(): array
-    {
-        // Consulta la información de la base de datos y realiza el cálculo
-        $capacities = Capacidad::all()->map(function ($capacidad) {
-            $capacidad->result = $capacidad->numero_maquinas * $capacidad->numero_maquinas;
-            return $capacidad;
-        });
-
-        // Depurar para verificar los datos
-        dd($capacities); 
-
-        // Asegúrate de retornar con el nombre correcto de la variable
-        return [
-            'capacities' => $capacities,
-        ];
-    }
-=======
         // Hacer que el widget ocupe todo el ancho
         protected int | string | array $columnSpan = 'full';
 
@@ -35,5 +17,4 @@ class CapacidadWidget extends Widget
             // Obtenemos todas las estaciones de trabajo desde la base de datos
             return Capacidad::all();
         }
->>>>>>> main
 }

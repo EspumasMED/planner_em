617f2d1455ead199a2d1397ada0774dc3f5bd6f5
Juan Caldera - Analista Ingenieria <analist-ing2@emedellin.lan>
2024-09-10 16:10:29 -0500

    se ajusta el import de time produccion

--- app/Imports/TiempoProduccionImport.php
+++ app/Imports/TiempoProduccionImport.php
@@ -12,22 +12,34 @@ class TiempoProduccionImport implements WithHeadingRow, ToCollection
     public function collection(Collection $rows)
     {
         foreach ($rows as $row) {
-            TiempoProduccion::create([
-                'referencia_colchon' => $row['referencia_colchon'],
-                'num_cierres' => $row['num_cierres'],
-                'fileteado_tapas' => $row['fileteado_tapas'],
-                'fileteado_falsos' => $row['fileteado_falsos'],
-                'maquina_rufflex' => $row['maquina_rufflex'],
-                'bordadora' => $row['bordadora'],
-                'decorado_falso' => $row['decorado_falso'],
-                'falso_pillow' => $row['falso_pillow'],
-                'encintado' => $row['encintado'],
-                'maquina_plana' => $row['maquina_plana'],
-                'marquillado' => $row['marquillado'],
-                'zona_pega' => $row['zona_pega'],
-                'cierre' => $row['cierre'],
-                'empaque' => $row['empaque'],
-            ]);
+            $data = [];
+
+            $fields = [
+                'referencia_colchon',
+                'num_cierres',
+                'fileteado_tapas',
+                'fileteado_falsos',
+                'maquina_rufflex',
+                'bordadora',
+                'decorado_falso',
+                'falso_pillow',
+                'encintado',
+                'maquina_plana',
+                'marquillado',
+                'zona_pega',
+                'cierre',
+                'empaque'
+            ];
+
+            foreach ($fields as $field) {
+                if (!empty($row[$field])) {
+                    $data[$field] = $row[$field];
+                }
+            }
+
+            if (!empty($data)) {
+                TiempoProduccion::create($data);
+            }
         }
     }
 }
